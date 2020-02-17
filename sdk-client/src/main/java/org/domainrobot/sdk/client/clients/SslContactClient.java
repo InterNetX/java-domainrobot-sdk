1c35dc45ed2caef29729886e706b7f7944927d63
Ephenodrom <30625794+Ephenodrom@users.noreply.github.com>
2020-02-17 12:05:05 +0100

    Add SslContact routes
    Added missing routes for the sslcontact object.

--- sdk-client/src/main/java/org/domainrobot/sdk/client/clients/SslContactClient.java
+++ sdk-client/src/main/java/org/domainrobot/sdk/client/clients/SslContactClient.java
@@ -1,5 +1,17 @@
 package org.domainrobot.sdk.client.clients;
 
+import java.util.List;
+import java.util.Map;
+
+import org.domainrobot.sdk.models.DomainrobotApiException;
+import org.domainrobot.sdk.models.generated.JsonResponseDataJsonNoData;
+import org.domainrobot.sdk.models.generated.JsonResponseDataSslContact;
+import org.domainrobot.sdk.models.generated.Query;
+import org.domainrobot.sdk.models.generated.SslContact;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -19,6 +31,102 @@ public class SslContactClient extends AbstractClient {
         this.baseUrl = baseUrl;
         this.version = version;
         this.template = template;
+    }
+
+    /**
+     * 
+     * Sends a sslcontact create request.
+     * 
+     * @return SslContact
+     * @throws DomainrobotApiException
+     */
+    public SslContact create(SslContact body, Map<String, String> customHeaders) throws DomainrobotApiException {
+        RequestEntity<SslContact> request = buildRequestEntity(body, HttpMethod.POST, baseUrl + "/sslcontact",
+                customHeaders);
+        ResponseEntity<JsonResponseDataSslContact> response = null;
+        try {
+            response = template.exchange(request, JsonResponseDataSslContact.class);
+        } catch (HttpClientErrorException e) {
+            handleException(e);
+        }
+        return response.getBody().getData().get(0);
+    }
+
+    /**
+     * 
+     * Sends a sslcontact update request.
+     * 
+     * @return SslContact
+     * @throws DomainrobotApiException
+     */
+    public SslContact update(SslContact body, Map<String, String> customHeaders) throws DomainrobotApiException {
+        if (body.getId() == null) {
+            throw new IllegalArgumentException("Field SslContact.id is missing.");
+        }
+        RequestEntity<SslContact> request = buildRequestEntity(body, HttpMethod.PUT,
+                baseUrl + "/sslcontact/" + body.getId().toString(), customHeaders);
+        ResponseEntity<JsonResponseDataSslContact> response = null;
+        try {
+            response = template.exchange(request, JsonResponseDataSslContact.class);
+        } catch (HttpClientErrorException e) {
+            handleException(e);
+        }
+        return response.getBody().getData().get(0);
+    }
+
+    /**
+     * 
+     * Sends a sslcontact delete request.
+     * 
+     * @throws DomainrobotApiException
+     */
+    public void delete(int id, Map<String, String> customHeaders) throws DomainrobotApiException {
+        RequestEntity<SslContact> request = buildRequestEntity(HttpMethod.DELETE, baseUrl + "/sslcontact/" + id,
+                customHeaders);
+        try {
+            template.exchange(request, JsonResponseDataJsonNoData.class);
+        } catch (HttpClientErrorException e) {
+            handleException(e);
+        }
+        return;
+    }
+
+    /**
+     * 
+     * Sends a sslcontact info request.
+     * 
+     * @return SslContact
+     * @throws DomainrobotApiException
+     */
+    public SslContact info(int id, Map<String, String> customHeaders) throws DomainrobotApiException {
+        RequestEntity<SslContact> request = buildRequestEntity(HttpMethod.GET, baseUrl + "/sslcontact/" + id,
+                customHeaders);
+        ResponseEntity<JsonResponseDataSslContact> response = null;
+        try {
+            response = template.exchange(request, JsonResponseDataSslContact.class);
+        } catch (HttpClientErrorException e) {
+            handleException(e);
+        }
+        return response.getBody().getData().get(0);
+    }
+
+    /**
+     * 
+     * Sends a sslcontact list request.
+     * 
+     * @return List of SslContact
+     * @throws DomainrobotApiException
+     */
+    public List<SslContact> list(Query body, Map<String, String> customHeaders) throws DomainrobotApiException {
+        RequestEntity<Query> request = buildRequestEntity(body, HttpMethod.GET, baseUrl + "/sslcontact/_search",
+                customHeaders);
+        ResponseEntity<JsonResponseDataSslContact> response = null;
+        try {
+            response = template.exchange(request, JsonResponseDataSslContact.class);
+        } catch (HttpClientErrorException e) {
+            handleException(e);
+        }
+        return response.getBody().getData();
     }
 
 }
